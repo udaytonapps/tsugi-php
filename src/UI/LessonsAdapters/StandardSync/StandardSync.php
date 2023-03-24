311757e97c2627f314bc046a2af037c94af84408
Andrew Niekamp <aniekamp1@udayton.edu>
2023-03-24 13:49:44 -0400

    TSUGI-514 Add general Sync adapter

--- src/UI/LessonsAdapters/StandardSync/StandardSync.php
+++ src/UI/LessonsAdapters/StandardSync/StandardSync.php
@@ -9,32 +9,97 @@
 
 namespace Tsugi\UI\StandardSync;
 
+use Badge;
+use Content;
+use LtiContent;
+
 class SyncCourse
 {
-    // /** @var string */
-    // public $title;
-    // /** @var string */
-    // public $description;
-    // /** @var Badge[] */
-    // public $badges;
-    // /** @var Module[] */
-    // public $modules;
+    /** @var string */
+    public $title;
+    /** @var string */
+    public $description;
+    /** @var Badge[] */
+    public $badges;
+    /** @var Module[] */
+    public $modules;
 
-    // function __construct($course)
-    // {
-    //     $this->title = $course->title;
-    //     $this->description = $course->description;
+    public $discussions; // TODO
 
-    //     $badges = array();
-    //     foreach ($course->badges as $badge) {
-    //         array_push($badges, new Badge($badge));
-    //     }
-    //     $this->badges = $badges;
+    function __construct($course)
+    {
+        $this->title = $course->title;
+        $this->description = $course->description;
 
-    //     $modules = array();
-    //     foreach ($course->modules as $module) {
-    //         array_push($modules, new Module($module));
-    //     }
-    //     $this->modules = $modules;
-    // }
+        $badges = array();
+        foreach ($course->badges as $badge) {
+            array_push($badges, new Badge($badge));
+        }
+        $this->badges = $badges;
+
+        $modules = array();
+        foreach ($course->modules as $module) {
+            array_push($modules, new SyncModule($module));
+        }
+        $this->modules = $modules;
+    }
+}
+
+class SyncModule
+{
+    /** @var string */
+    public $title;
+    /** @var string */
+    public $description;
+    /** @var string */
+    public $session;
+    /** @var string */
+    public $anchor;
+    /** @var string */
+    public $duration;
+    /** @var string */
+    public $image;
+    /** @var string */
+    public $icon;
+    /** @var string */
+    public $calendar;
+    // /** @var Content[] Typically a title, description, and video */
+    // public $landingContents;
+    /** @var LtiContent[] */
+    public $lti;
+    //   "core": true,
+    public $learningoutcomes;
+    public $facilitators;
+    public $resources;
+
+    function __construct($module)
+    {
+        $this->title = $module->title ?? null;
+        $this->description = $module->description ?? null;
+        $this->session = $module->session ?? null;
+        $this->anchor  = $module->anchor ?? null;
+        $this->duration  = $module->duration ?? null;
+        $this->image = $module->image ?? null;
+        $this->icon = $module->icon ?? null;
+        $this->calendar = $module->calendar ?? null;
+        $this->learningoutcomes = $module->learningoutcomes ?? null;
+        $this->facilitators = $module->facilitators ?? null;
+        $this->resources = $module->resources ?? null;
+
+        $contents = [];
+        // if (isset($module->landingContents)) {
+        //     foreach ($module->landingContents as $content) {
+        //         $newLesson = new Content($content);
+        //         array_push($contents, $newLesson);
+        //     }
+        // }
+        $ltiContent = [];
+        if (isset($module->lti)) {
+            foreach ($module->lti as $lti) {
+                $newLti = new LtiContent($lti);
+                $ltiContent[] = $newLti;
+            }
+        }
+        $this->lti = $ltiContent;
+    }
 }
